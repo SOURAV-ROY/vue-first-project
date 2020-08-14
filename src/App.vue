1f2636d6174f15391c044e3df14383186209f734
SOURAV ROY <souravroy.info13@gmail.com>
2020-08-14 12:49:52 +0600

    Animating the Route Transitions 😊

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <app-header></app-header>
     <hr>
-    <router-view></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -25,12 +27,33 @@ body {
   padding: 30px;
 }
 
-/*#app {*/
-/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-/*  -webkit-font-smoothing: antialiased;*/
-/*  -moz-osx-font-smoothing: grayscale;*/
-/*  text-align: center;*/
-/*  color: #2c3e50;*/
-/*  margin-top: 60px;*/
-/*}*/
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
